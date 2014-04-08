881252fb8443e6d9d6858ce5abc5fe0fd67eff75
Matt Policastro <mjpolicas@gmail.com>
2014-04-08 17:04:53 -0400

    Updating stylesheets, .gitignore

--- site/templates/blogarticle.php
+++ site/templates/blogarticle.php
@@ -1,10 +1,13 @@
 <?php snippet('header') ?>
 
-<section class="content blogarticle">
+<section class="content blog">
 
 	<article>
 		<h1><?php echo html($page->title()) ?></h1>
 		<?php echo kirbytext($page->text()) ?>
+		
+		<time datetime="<?php echo $page->date('c') ?>" pubdate="pubdate"><?php echo $page->date('d.m.y g:i a T') ?></time>
+		
 		<a href="<?php echo url('blog') ?>">Back to blog.</a>
 	</article>
 
